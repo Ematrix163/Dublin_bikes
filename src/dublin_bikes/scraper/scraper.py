f95d39b4008810998263375c502db8e36fd41473
diarmuidmorgan <diarmuidmorgan@hotmail.com>
2018-02-27 21:10:30 +0000

    saving data destroyed my AWS! took local writes out of scraper.py

--- src/dublin_bikes/scraper/scraper.py
+++ src/dublin_bikes/scraper/scraper.py
@@ -47,21 +47,21 @@ def scrape():
     while True:
         #run this forever
         t=time.time()
-        f=open('data_in_text_form/'+str(time.time()) + '_bikedata.txt', 'w')
-        data = requests.get('https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=80b7eeead410f23a165f6d67bc9d33e514e8ee84')
-        f.write(data.text)
 
-        f.close()
+        data = requests.get('https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=80b7eeead410f23a165f6d67bc9d33e514e8ee84')
+
+
+
         data = json.loads(data.text)
         insertDB(data, t)
 
 
         #Here we're getting the weather data. Currently not uploading this
         #to db, as I have no idea what the table should look like
-        f=open('data_in_text_form/'+str(time.time()) + '_weather_data.txt', 'w')
+
         data = requests.get('http://api.openweathermap.org/data/2.5/find?q=Dublin&units=imperial&type=accurate&mode=json&APPID=def5ec12072a2e8060e27a30bdbebb2e')
-        f.write(data.text)
-        f.close()
+
+
         time.sleep(300)
 
 if __name__ == '__main__':
